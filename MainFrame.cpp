d6ef81cd661616f51a6a7023c40eb0afc1821fdb
Arpan Raval <arpanraval1112@gmail.com>
2025-01-04 21:54:20 +0530

    Add gauge progressbar

--- MainFrame.cpp
+++ MainFrame.cpp
@@ -12,4 +12,7 @@ MainFrame::MainFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title) 
 	wxTextCtrl* textBox = new wxTextCtrl(panel, wxID_ANY, "You Can Change This", wxPoint(50, 250), wxSize(200, 20));
 
 	wxSlider* slider = new wxSlider(panel, wxID_ANY, 25, 0, 100, wxPoint(50, 350), wxSize(200, -1));
+
+	wxGauge* gauge = new wxGauge(panel, wxID_ANY, 100, wxPoint(550, 350), wxSize(200, -1));
+	gauge->SetValue(50);
 }
