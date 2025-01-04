db71105719bded020539225a5c89aa73a01c3048
Arpan Raval <arpanraval1112@gmail.com>
2025-01-04 19:39:53 +0530

    Add project files.

---
+++ MainFrame.cpp
@@ -0,0 +1,9 @@
+#include "MainFrame.h"
+#include <wx/wx.h>
+
+MainFrame::MainFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title) {
+	wxPanel* panel = new wxPanel(this);
+	wxButton* button = new wxButton(panel, wxID_ANY, "Button", wxPoint(50, 50), wxSize(100, 35));
+
+	wxCheckBox* checkbox = new wxCheckBox(panel, wxID_ANY, "CheckBox",wxPoint(550,55));
+} 
