5b10be64c9bf14f29024a570fa7f03d28c597ef1
Arpan Raval <arpanraval1112@gmail.com>
2025-01-04 20:14:39 +0530

    Add static text

--- MainFrame.cpp
+++ MainFrame.cpp
@@ -6,4 +6,6 @@ MainFrame::MainFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title) 
 	wxButton* button = new wxButton(panel, wxID_ANY, "Button", wxPoint(50, 50), wxSize(100, 35));
 
 	wxCheckBox* checkbox = new wxCheckBox(panel, wxID_ANY, "CheckBox",wxPoint(550,55));
+
+	wxStaticText* staticText = new wxStaticText(panel, wxID_ANY, "I Can't Move From Here", wxPoint(50, 150));
 } 
