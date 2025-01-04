556f3c8f92b5f51e2dafd401ccfcee06079ffcf6
Arpan Raval <arpanraval1112@gmail.com>
2025-01-04 21:28:38 +0530

    Add Slider

--- MainFrame.cpp
+++ MainFrame.cpp
@@ -5,9 +5,11 @@ MainFrame::MainFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title) 
 	wxPanel* panel = new wxPanel(this);
 	wxButton* button = new wxButton(panel, wxID_ANY, "Button", wxPoint(50, 50), wxSize(100, 35));
 
-	wxCheckBox* checkbox = new wxCheckBox(panel, wxID_ANY, "CheckBox",wxPoint(550,55));
+	wxCheckBox* checkbox = new wxCheckBox(panel, wxID_ANY, "CheckBox",wxPoint(550,50));
 
 	wxStaticText* staticText = new wxStaticText(panel, wxID_ANY, "I Can't Move From Here", wxPoint(50, 150));
 
-	wxTextCtrl* textBox = new wxTextCtrl(panel, wxID_ANY, "You Can Change This", wxPoint(50, 200), wxSize(200, 20));
+	wxTextCtrl* textBox = new wxTextCtrl(panel, wxID_ANY, "You Can Change This", wxPoint(50, 250), wxSize(200, 20));
+
+	wxSlider* slider = new wxSlider(panel, wxID_ANY, 25, 0, 100, wxPoint(50, 350), wxSize(200, 50));
 } 
