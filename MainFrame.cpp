cd894e201ac56e4a5dd51fa233142834bc277145
Arpan Raval <arpanraval1112@gmail.com>
2025-01-06 22:11:28 +0530

    align text to left side in a button

--- MainFrame.cpp
+++ MainFrame.cpp
@@ -4,7 +4,7 @@
 
 MainFrame::MainFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title) {
 	wxPanel* panel = new wxPanel(this);  //Without This ,Button Will Take A FullScreen So we Have to Adjust In Panel.
-	wxButton* button = new wxButton(panel, wxID_ANY, "Button", wxPoint(50, 50), wxSize(100, 35)); //Normal Button
+	wxButton* button = new wxButton(panel, wxID_ANY, "Button", wxPoint(50, 50), wxSize(100, 35), wxBU_LEFT); //Normal Button
 
 	wxCheckBox* checkbox = new wxCheckBox(panel, wxID_ANY, "CheckBox",wxPoint(550,50));  //Tick Mark Karva Mate
 
