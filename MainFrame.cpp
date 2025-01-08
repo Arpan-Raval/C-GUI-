02f4da568b27f13e6e0895581c7a8a385e57dbf5
Arpan Raval <arpanraval1112@gmail.com>
2025-01-08 23:57:18 +0530

    make event table to make it perfect

--- MainFrame.cpp
+++ MainFrame.cpp
@@ -4,9 +4,18 @@
 enum {
 	BUTTON_ID = 2  // give id to button
 };
+
+wxBEGIN_EVENT_TABLE(MainFrame,wxFrame)
+     EVT_BUTTON(BUTTON_ID,MainFrame::Submit)      //aa naa lakho to button work naa thay , etle event table banavu J pade
+wxEND_EVENT_TABLE()
+
 MainFrame::MainFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title) {
 	wxPanel* panel = new wxPanel(this);  //Without This ,Button Will Take A FullScreen So we Have to Adjust In Panel.
 
 	wxButton* button = new wxButton(panel, BUTTON_ID, "Button", wxPoint(300, 250), wxSize(200, 50));
 	CreateStatusBar();  // this will create status bar below 
+}
+
+void MainFrame::Submit(wxCommandEvent& evt) {
+	wxLogStatus("Button Clicked!!!");           // event function
 }
