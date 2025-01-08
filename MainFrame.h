3859680eb2383a582bc38b64d7404f8f6536c6ce
Arpan Raval <arpanraval1112@gmail.com>
2025-01-08 23:56:52 +0530

    declare event table

--- MainFrame.h
+++ MainFrame.h
@@ -5,5 +5,8 @@ class MainFrame : public wxFrame
 {
 public:
 	MainFrame(const wxString& title);
+private:
+	void Submit(wxCommandEvent& evt);  //make event
+	wxDECLARE_EVENT_TABLE();    // this will create a event table for sync with button
 };
 
