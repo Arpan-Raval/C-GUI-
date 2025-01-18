5d60faee37738f9db7159f3a23e67f5157fdf5d5
Arpan Raval <arpanraval1112@gmail.com>
2025-01-18 19:16:06 +0530

    Add Bind method and everything works as older one without event table

--- MainFrame.cpp
+++ MainFrame.cpp
@@ -8,6 +8,10 @@ MainFrame::MainFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title) 
 	wxButton* button = new wxButton(panel,wxID_ANY, "Button", wxPoint(300, 250), wxSize(200, 50));
 	wxSlider* slider = new wxSlider(panel,wxID_ANY, 25, 0, 100, wxPoint(300, 350), wxSize(200, 50));
 	wxTextCtrl* text = new wxTextCtrl(panel,wxID_ANY, "", wxPoint(300, 150), wxSize(200, 25));
+
+	button->Bind(wxEVT_BUTTON, &MainFrame::Submit, this);
+	slider->Bind(wxEVT_SLIDER, &MainFrame::Slider, this);
+	text->Bind(wxEVT_TEXT, &MainFrame::OnText, this);
 	CreateStatusBar();  // this will create status bar below 
 }
 
