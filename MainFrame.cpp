c760d6a3e43c190a6e9365acbc6480a05be3f08c
Arpan Raval <arpanraval1112@gmail.com>
2025-01-18 19:07:08 +0530

    Delete id and add any id

--- MainFrame.cpp
+++ MainFrame.cpp
@@ -1,18 +1,13 @@
 #include "MainFrame.h"
 #include <wx/wx.h>
 
-enum {
-	BUTTON_ID = 2,  // give id to button
-	SLIDER_ID = 3,  // GIVE ID TO SLIDER
-	TEXT_ID = 4
-};
 
 MainFrame::MainFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title) {
 	wxPanel* panel = new wxPanel(this);  //Without This ,Button Will Take A FullScreen So we Have to Adjust In Panel.
 
-	wxButton* button = new wxButton(panel, BUTTON_ID, "Button", wxPoint(300, 250), wxSize(200, 50));
-	wxSlider* slider = new wxSlider(panel, SLIDER_ID, 25, 0, 100, wxPoint(300, 350), wxSize(200, 50));
-	wxTextCtrl* text = new wxTextCtrl(panel, TEXT_ID, "", wxPoint(300, 150), wxSize(200, 25));
+	wxButton* button = new wxButton(panel,wxID_ANY, "Button", wxPoint(300, 250), wxSize(200, 50));
+	wxSlider* slider = new wxSlider(panel,wxID_ANY, 25, 0, 100, wxPoint(300, 350), wxSize(200, 50));
+	wxTextCtrl* text = new wxTextCtrl(panel,wxID_ANY, "", wxPoint(300, 150), wxSize(200, 25));
 	CreateStatusBar();  // this will create status bar below 
 }
 
