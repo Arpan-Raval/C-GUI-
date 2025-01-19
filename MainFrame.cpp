3ad0f188af3b0e2a21fe8883f3a1a12f93584f58
Arpan Raval <arpanraval1112@gmail.com>
2025-01-19 13:45:09 +0530

    Delete Everything for learning Mouse Events

--- MainFrame.cpp
+++ MainFrame.cpp
@@ -5,46 +5,6 @@
 MainFrame::MainFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title) {
 	wxPanel* panel = new wxPanel(this);  //Without This ,Button Will Take A FullScreen So we Have to Adjust In Panel.
 
-	wxButton* button1 = new wxButton(panel,wxID_ANY, "Button", wxPoint(300, 250), wxSize(200, 50));
-	wxButton* button2 = new wxButton(panel, wxID_ANY, "Button2", wxPoint(300, 300), wxSize(200, 50));
-	wxSlider* slider = new wxSlider(panel,wxID_ANY, 25, 0, 100, wxPoint(300, 350), wxSize(200, 50));
-	wxTextCtrl* text = new wxTextCtrl(panel,wxID_ANY, "", wxPoint(300, 150), wxSize(200, 25));
-
-	this->Bind(wxEVT_BUTTON, &MainFrame::Submit, this);        //  this ma lakhavathi mainframe ma jetla pan button hashe e badha ma aa event run thashe
-	button1->Bind(wxEVT_BUTTON, &MainFrame::onButton1, this);  //button1 ma khali e button upar nu function run thashe
-	button2->Bind(wxEVT_BUTTON, &MainFrame::onButton2, this);
-	slider->Bind(wxEVT_SLIDER, &MainFrame::Slider, this);
-	text->Bind(wxEVT_TEXT, &MainFrame::OnText, this);
-
-	this->Bind(wxEVT_CLOSE_WINDOW, &MainFrame::onClose, this);  // This is for closing a window
-
+	
 	CreateStatusBar();  // this will create status bar below 
-}
-
-void MainFrame::Submit(wxCommandEvent& evt) {
-	wxLogMessage("Button Clicked!!!");           // event function
-}
-
-void MainFrame::onButton1(wxCommandEvent& evt) {
-	wxLogStatus("button 1 Clicked");
-	evt.Skip();       // aanathi aa skip thayi jashe and aana sivay pan je function hashe e button upar e run thashe (ex. button1 upar have submit and onbutton1 em banne function run thashe)
-}														//these 2 are new event function which will run
-
-void MainFrame::onButton2(wxCommandEvent& evt) {
-	wxLogStatus("Button 2 Clicked");
-	evt.Skip();
-}
-void MainFrame::Slider(wxCommandEvent& evt) {
-	wxString str = wxString::Format("Slider Value is : %d", evt.GetInt());
-	wxLogStatus(str);
-}
-
-void MainFrame::OnText(wxCommandEvent& evt) {
-	wxString str = wxString::Format("text is : %s", evt.GetString());
-	wxLogStatus(str);
-}
-
-void MainFrame::onClose(wxCloseEvent& evt) {
-	wxLogMessage("CLosing a Window");             // it makes a pop up message when we click close button
-	evt.Skip();                                   //because its parent node doesn't know that we actually want to close the window ,,without this we only get message but the window will still open.
 }
--- MainFrame.h
+++ MainFrame.h
@@ -5,14 +5,6 @@ class MainFrame : public wxFrame
 {
 public:
 	MainFrame(const wxString& title);
-private:
-	void Submit(wxCommandEvent& evt);  //make event
-	void onButton1(wxCommandEvent& evt);
-	void onButton2(wxCommandEvent& evt);
-	
-	void onClose(wxCloseEvent& evt);    // for closing a event
 
-	void Slider(wxCommandEvent& evt);
-	void OnText(wxCommandEvent& evt);
 };
 
