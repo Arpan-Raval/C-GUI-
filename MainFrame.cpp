7bf0f5ac4204ccfed8c6ded8481c4c865f10bef5
Arpan Raval <arpanraval1112@gmail.com>
2025-01-19 13:55:29 +0530

    Add Simple Mouse Event

--- MainFrame.cpp
+++ MainFrame.cpp
@@ -7,4 +7,10 @@ MainFrame::MainFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title) 
 
 	
 	CreateStatusBar();  // this will create status bar below 
+    
+	panel->Bind(wxEVT_LEFT_DOWN, &MainFrame::onMouse, this);    // aanathi jyare pan screen par click karshu etle function run thashe and print karshe
+}
+
+void MainFrame::onMouse(wxMouseEvent& evt) {
+	wxLogStatus("Mouse Event Detected");         // status bar ma print karse aavu
 }
--- MainFrame.h
+++ MainFrame.h
@@ -5,6 +5,7 @@ class MainFrame : public wxFrame
 {
 public:
 	MainFrame(const wxString& title);
-
+public:
+	void onMouse(wxMouseEvent& evt);
 };
 
