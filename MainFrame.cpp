30a920736bc181139765ec36b9c70945ef12970e
Arpan Raval <arpanraval1112@gmail.com>
2025-01-19 23:02:16 +0530

    tab and arrow key functionality

--- MainFrame.cpp
+++ MainFrame.cpp
@@ -4,6 +4,8 @@
 
 MainFrame::MainFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title) {
 	wxPanel* panel = new wxPanel(this);  //Without This ,Button Will Take A FullScreen So we Have to Adjust In Panel.
+	wxButton* btn1 = new wxButton(panel, wxID_ANY, "button", wxPoint(200, 100), wxSize(200, 25)); //in this buttons ,it will select another button when we click tab or arrow keys
+	wxButton* btn2 = new wxButton(panel, wxID_ANY, "button", wxPoint(200, 300), wxSize(200, 25));
 
 	panel->Bind(wxEVT_KEY_DOWN,&MainFrame::onKey, this);  //keyboard ni key press karva upar function Run Thashe
 
